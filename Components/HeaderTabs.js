e090b48f414f00b48aee99d72ccdb7591e380b49
Victorvikson1996 <53367180+Victorvikson1996@users.noreply.github.com>
2021-10-10 22:29:32 +0530

    items Components

--- Components/HeaderTabs.js
+++ Components/HeaderTabs.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 
 export default function HeaderTabs() {
+
+    const [activeTab, setActiveTab] = useState('Delivery')
     return (
         <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
-            <HeaderButton text="Delivery" />
-            <HeaderButton text="Pickup" />
+            <HeaderButton
+                text="Delivery"
+                btnColor="black"
+                textColor="white"
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+            />
+            <HeaderButton
+                text="Pickup"
+                btnColor="white"
+                textColor="black"
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+            />
         </View>
     )
 }
@@ -13,8 +27,17 @@ export default function HeaderTabs() {
 
 const HeaderButton = (props) => {
     return (
-        <TouchableOpacity style={{ backgroundColor: 'black' }}>
-            <Text style={{ color: 'white' }}>{props.text}</Text>
+        <TouchableOpacity
+            style={{
+                backgroundColor: props.activeTab === props.text ? "black" : 'white',
+                paddingVertical: 6,
+                paddingHorizontal: 16,
+                borderRadius: 30,
+                marginTop: 10,
+            }}
+            onPress={() => props.setActiveTab(props.text)}
+        >
+            <Text style={{ color: props.activeTab === props.text ? 'white' : "black" }}>{props.text}</Text>
         </TouchableOpacity>
     )
 }
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
+import Categories from '../Components/Categories'
 import HeaderTabs from '../Components/HeaderTabs'
+import SearchBar from '../Components/SearchBar'
 
 export default function Home() {
     return (
-        <SafeAreaView>
-            <HeaderTabs />
+        <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+            <View style={{ backgroundColor: 'white', padding: 15 }}>
+                <HeaderTabs />
+                <SearchBar />
+            </View>
+            <Categories />
         </SafeAreaView>
     )
 }
