5769f52163662feb3e8c0d55c6e379d5c7f26155
Ezea Victor Chukwuebuka <53367180+Victorvikson1996@users.noreply.github.com>
2021-11-06 22:31:03 +0530

    bottom tabs

---
+++ Components/BottomTabs.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+export default function BottomTabs() {
+  return (
+    <View>
+      <Text>Bottom</Text>
+    </View>
+  );
+}
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,59 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import Categories from '../Components/Categories'
-import HeaderTabs from '../Components/HeaderTabs'
-import SearchBar from '../Components/SearchBar'
-import ResturantItems, { LocalResturants } from '../Components/ResturantItems'
+import React, { useState, useEffect } from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import Categories from "../Components/Categories";
+import HeaderTabs from "../Components/HeaderTabs";
+import SearchBar from "../Components/SearchBar";
+import ResturantItems, { LocalResturants } from "../Components/ResturantItems";
+import { Divider } from "react-native-elements";
+import BottomTabs from "../Components/BottomTabs";
 
-
-
-const YELP_API_KEY = "_R6U1T2m25B07OxYcxTu2qwTeaqvyEQVKVo5-jizULMbgUNYGpOXt9aixO9PSPPbZ88NPQym2NWJ4VTTbDsh9tdneBmTcLekU_OMERPLt153p-xGs_9x5-0zx9o5X3Yx"
+const YELP_API_KEY =
+  "_R6U1T2m25B07OxYcxTu2qwTeaqvyEQVKVo5-jizULMbgUNYGpOXt9aixO9PSPPbZ88NPQym2NWJ4VTTbDsh9tdneBmTcLekU_OMERPLt153p-xGs_9x5-0zx9o5X3Yx";
 
 export default function Home() {
+  const [resturantData, setResturantData] = useState(LocalResturants);
+  const [city, setCity] = useState("San Francisco");
+  const [activeTab, setActiveTab] = useState("Delivery");
 
-    const [resturantData, setResturantData] = useState(LocalResturants)
-    const [city, setCity] = useState("San Francisco");
-    const [activeTab, setActiveTab] = useState("Delivery");
+  const getResturantFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
 
-    const getResturantFromYelp = () => {
-        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
 
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) =>
+        setResturantData(
+          json.businesses.filter((bussines) =>
+            bussines.transactions.includes(activeTab.toLowerCase())
+          )
+        )
+      );
+  };
 
+  useEffect(() => {
+    getResturantFromYelp();
+  }, [city, activeTab]);
 
-        const apiOptions = {
-            headers: {
-                Authorization: `Bearer ${YELP_API_KEY}`,
-            },
-        };
-
-        return fetch(yelpUrl, apiOptions)
-            .then((res) => res.json())
-            .then((json) => setResturantData(
-                json.businesses.filter((bussines) =>
-                    bussines.transactions.includes(activeTab.toLowerCase())
-                )
-            ));
-    }
-
-
-    useEffect(() => {
-        getResturantFromYelp()
-    }, [city, activeTab])
-
-
-    return (
-        <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
-            <View style={{ backgroundColor: 'white', padding: 15 }}>
-                <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
-                <SearchBar cityHandler={setCity} />
-            </View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <Categories />
-                <ResturantItems
-                    resturantData={resturantData}
-                />
-            </ScrollView>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        <SearchBar cityHandler={setCity} />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <ResturantItems resturantData={resturantData} />
+      </ScrollView>
+      <Divider width={1} />
+      <BottomTabs />
+    </SafeAreaView>
+  );
 }
-
