db62833171dabeb25da2b112ae2806fb85242bfd
Ezea Victor Chukwuebuka <53367180+Victorvikson1996@users.noreply.github.com>
2021-11-06 23:22:33 +0530

    bottom tabs Icons

--- Components/BottomTabs.js
+++ Components/BottomTabs.js
@@ -1,9 +1,38 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+
 export default function BottomTabs() {
   return (
-    <View>
-      <Text>Bottom</Text>
+    <View
+      style={{
+        flexDirection: "row",
+        margin: 10,
+        marginHorizontal: 30,
+        justifyContent: "space-between",
+      }}
+    >
+      <Icon icon="home" text="Home" />
+      <Icon icon="search" text="Browse" />
+      <Icon icon="shopping-bag" text="Glocery" />
+      <Icon icon="receipt" text="Orders" />
+      <Icon icon="user" text="Account" />
     </View>
   );
 }
+
+const Icon = (props) => {
+  return (
+    <View>
+      <FontAwesome5
+        name={props.icon}
+        size={25}
+        style={{
+          margin: 3,
+          alignSelf: "center",
+        }}
+      />
+      <Text>{props.text}</Text>
+    </View>
+  );
+};
